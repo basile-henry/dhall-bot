1ee55a51495df035796f17b66d5744235b187b95
Basile Henry <bjm.henry@gmail.com>
2019-01-28 19:13:27 +0700

    Use a more recent version of dhall

--- default.nix
+++ default.nix
@@ -2,5 +2,14 @@
 
 with nixpkgs.pkgs.haskell;
 
+let dhall-haskell = import (builtins.fetchGit {
+        name = "dhall-haskell";
+        url = https://github.com/dhall-lang/dhall-haskell/;
+        rev = "2d2c897e3f43a9b1188ae82e7a1da5b5218fb135";
+        ref = "master";
+    });
+in
+
 lib.failOnAllWarnings (packages.ghc863.callCabal2nix "dhall-bot" ./.  {
+    dhall = dhall-haskell.dhall;
 })
