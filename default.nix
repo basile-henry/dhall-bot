3f750991f43f83bfeed7e45b269ed8161c760c7a
Basile Henry <bjm.henry@gmail.com>
2019-01-30 10:03:05 +0700

    Use dhall as a library, not a binary

--- default.nix
+++ default.nix
@@ -2,15 +2,17 @@
 
 with nixpkgs.pkgs.haskell;
 
-let dhall-haskell = import (nixpkgs.fetchgit {
+let dhall-src = nixpkgs.fetchgit {
         name = "dhall-haskell";
         url = https://github.com/dhall-lang/dhall-haskell/;
         rev = "3fbd0d72f85340ddcaa1c5b1ec41da768466df35";
         sha256 = "1vk3cp1dkg6zaw202nxiar3hjx237lhm68r6cpvfpl7jmvgm9b6j";
         fetchSubmodules = true;
-    });
+    } + /dhall;
+
+    dhall = lib.dontCheck (packages.ghc863.callCabal2nix "dhall" dhall-src {});
 in
 
 lib.failOnAllWarnings (packages.ghc863.callCabal2nix "dhall-bot" ./.  {
-    dhall = dhall-haskell.dhall;
+    inherit dhall;
 })
